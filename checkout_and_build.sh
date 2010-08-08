510ddc2b8a398afeafe4f8600637f5afbdcc0ac6
rlgomes <rodnegomes@gmail.com>
2010-08-08 19:17:13 +0000

    - some fixes to the checkout script

--- checkout_and_build.sh
+++ checkout_and_build.sh
@@ -1,45 +1,43 @@
 #!/bin/bash
 
-DATE=`date +%d-%m-%Y.%H:%M:%S`
+BASE=/home/dtftest
 
-rm -fr build
-# none of those files are required to upload the new ones
-rm -fr results/*
+rm -fr $BASE/build
 
-mkdir build
-mkdir build/logs
+mkdir $BASE/build
+mkdir $BASE/build/logs
 
-cd build
+cd $BASE/build
 
 echo "Pulling DTF..."
-git clone ssh://git@github.com/rlgomes/dtf.git > ~/build/logs/clone.log 2>&1
+git clone ssh://git@github.com/rlgomes/dtf.git > $BASE/build/logs/clone.log 2>&1
 
 echo "Building DTF..."
 cd dtf
-ant clean > ~/build/logs/clean.log 2>&1
-ant build >> ~/build/logs/build.log 2>&1
+ant clean > $BASE/build/logs/clean.log 2>&1
+ant build >> $BASE/build/logs/build.log 2>&1
 
 echo "Running JUnit tests..."
-ant junit > ~/build/logs/junit.log 2>&1
+ant junit > $BASE/build/logs/junit.log 2>&1
 
 cd build/dtf/dist
 echo "Running DTF unit tests..."
-./ant.sh run_ut > ~/build/logs/ut.log 2>&1
+./ant.sh run_ut > $BASE/build/logs/ut.log 2>&1
 
-cd ~/build/dtf
+cd $BASE/build/dtf
 
 echo "Generating test reports..."
-ant junit-reports > ~/build/logs/junit-reports.log 2>&1
+ant junit-reports > $BASE/build/logs/junit-reports.log 2>&1
 
 echo "Copying logs/results to results repository..."
 
-cp -fr ~/build/logs ~/gh-pages/results/
-cp -fr ~/build/dtf/build/dtf/reports/* ~/gh-pages/results/
-cp -fr ~/build/dtf/build/dtf/junit_results ~/gh-pages/results/
-cp ~/build/dtf/build/dtf/dist/tests/ut/output/TEST_ut_results.xml ~/gh-pages/results/
-cp ~/build/dtf/build/dtf/dist/tests/ut/output/script-ut.out ~/gh-pages/results/
+cp -fr $BASE/build/logs $BASE/gh-pages/results/
+cp -fr $BASE/build/dtf/build/dtf/reports/* $BASE/gh-pages/results/
+cp -fr $BASE/build/dtf/build/dtf/junit_results $BASE/gh-pages/results/
+cp $BASE/build/dtf/build/dtf/dist/tests/ut/output/TEST_ut_results.xml $BASE/gh-pages/results/
+cp $BASE/build/dtf/build/dtf/dist/tests/ut/output/script-ut.out $BASE/gh-pages/results/
 
-cd ~/gh-pages
+cd $BASE/gh-pages
 git add results
 git commit -m "test results for $DATE"
 git push origin gh-pages
