61858b46125f466a167989be0c6a9fa6ebffe884
dtftest <dtftest@localhost.(none)>
2011-01-01 07:46:30 +0000

    Test results for commit 92bddc215864b0e6fa7efe7d261df3019831543f- on 01-01-2011.07:45:38

--- results/perf/pullcharts.sh
+++ results/perf/pullcharts.sh
@@ -3,140 +3,140 @@
 
 # chart for xml.xpath.complex_xpath.access
 echo "getting chart for xml.xpath.complex_xpath.access"
-./createlc.py '[("xml.xpath.complex_xpath.access",[9223372036854775807])]' '["92bddc215864b0e6fa7efe7d261df3019831543f-"]' xml.xpath.complex_xpath.access.png
+./createlc.py '[("xml.xpath.complex_xpath.access",[9223372036854775807,9223372036854775807])]' '["92bddc215864b0e6fa7efe7d261df3019831543f-","92bddc215864b0e6fa7efe7d261df3019831543f-"]' xml.xpath.complex_xpath.access.png
 
 # chart for xml.xpath.simple_xpath.access
 echo "getting chart for xml.xpath.simple_xpath.access"
-./createlc.py '[("xml.xpath.simple_xpath.access",[9223372036854775807])]' '["92bddc215864b0e6fa7efe7d261df3019831543f-"]' xml.xpath.simple_xpath.access.png
+./createlc.py '[("xml.xpath.simple_xpath.access",[9223372036854775807,9223372036854775807])]' '["92bddc215864b0e6fa7efe7d261df3019831543f-","92bddc215864b0e6fa7efe7d261df3019831543f-"]' xml.xpath.simple_xpath.access.png
 
 # chart for http.delete
 echo "getting chart for http.delete"
-./createlc.py '[("http.delete",[1063.830])]' '["92bddc215864b0e6fa7efe7d261df3019831543f-"]' http.delete.png
+./createlc.py '[("http.delete",[1063.830,1063.830])]' '["92bddc215864b0e6fa7efe7d261df3019831543f-","92bddc215864b0e6fa7efe7d261df3019831543f-"]' http.delete.png
 
 # chart for http.head
 echo "getting chart for http.head"
-./createlc.py '[("http.head",[709.220])]' '["92bddc215864b0e6fa7efe7d261df3019831543f-"]' http.head.png
+./createlc.py '[("http.head",[709.220,724.638])]' '["92bddc215864b0e6fa7efe7d261df3019831543f-","92bddc215864b0e6fa7efe7d261df3019831543f-"]' http.head.png
 
 # chart for http.get
 echo "getting chart for http.get"
-./createlc.py '[("http.get",[662.252])]' '["92bddc215864b0e6fa7efe7d261df3019831543f-"]' http.get.png
+./createlc.py '[("http.get",[662.252,925.926])]' '["92bddc215864b0e6fa7efe7d261df3019831543f-","92bddc215864b0e6fa7efe7d261df3019831543f-"]' http.get.png
 
 # chart for http.put
 echo "getting chart for http.put"
-./createlc.py '[("http.put",[476.190])]' '["92bddc215864b0e6fa7efe7d261df3019831543f-"]' http.put.png
+./createlc.py '[("http.put",[476.190,384.615])]' '["92bddc215864b0e6fa7efe7d261df3019831543f-","92bddc215864b0e6fa7efe7d261df3019831543f-"]' http.put.png
 
 # chart for http.post
 echo "getting chart for http.post"
-./createlc.py '[("http.post",[281.690])]' '["92bddc215864b0e6fa7efe7d261df3019831543f-"]' http.post.png
+./createlc.py '[("http.post",[281.690,282.486])]' '["92bddc215864b0e6fa7efe7d261df3019831543f-","92bddc215864b0e6fa7efe7d261df3019831543f-"]' http.post.png
 
 # chart for rendezvous.remote.visits
 echo "getting chart for rendezvous.remote.visits"
-./createlc.py '[("rendezvous.remote.visits",[100])]' '["92bddc215864b0e6fa7efe7d261df3019831543f-"]' rendezvous.remote.visits.png
+./createlc.py '[("rendezvous.remote.visits",[100,100])]' '["92bddc215864b0e6fa7efe7d261df3019831543f-","92bddc215864b0e6fa7efe7d261df3019831543f-"]' rendezvous.remote.visits.png
 
 # chart for rendezvous.local.visits
 echo "getting chart for rendezvous.local.visits"
-./createlc.py '[("rendezvous.local.visits",[100])]' '["92bddc215864b0e6fa7efe7d261df3019831543f-"]' rendezvous.local.visits.png
+./createlc.py '[("rendezvous.local.visits",[100,100])]' '["92bddc215864b0e6fa7efe7d261df3019831543f-","92bddc215864b0e6fa7efe7d261df3019831543f-"]' rendezvous.local.visits.png
 
 # chart for share.cumulative.local.syncs
 echo "getting chart for share.cumulative.local.syncs"
-./createlc.py '[("share.cumulative.local.syncs",[100])]' '["92bddc215864b0e6fa7efe7d261df3019831543f-"]' share.cumulative.local.syncs.png
+./createlc.py '[("share.cumulative.local.syncs",[100,100])]' '["92bddc215864b0e6fa7efe7d261df3019831543f-","92bddc215864b0e6fa7efe7d261df3019831543f-"]' share.cumulative.local.syncs.png
 
 # chart for share.cumulative.remote.syncs
 echo "getting chart for share.cumulative.remote.syncs"
-./createlc.py '[("share.cumulative.remote.syncs",[100])]' '["92bddc215864b0e6fa7efe7d261df3019831543f-"]' share.cumulative.remote.syncs.png
+./createlc.py '[("share.cumulative.remote.syncs",[100,100])]' '["92bddc215864b0e6fa7efe7d261df3019831543f-","92bddc215864b0e6fa7efe7d261df3019831543f-"]' share.cumulative.remote.syncs.png
 
 # chart for share.queue.local.syncs
 echo "getting chart for share.queue.local.syncs"
-./createlc.py '[("share.queue.local.syncs",[100])]' '["92bddc215864b0e6fa7efe7d261df3019831543f-"]' share.queue.local.syncs.png
+./createlc.py '[("share.queue.local.syncs",[100,100])]' '["92bddc215864b0e6fa7efe7d261df3019831543f-","92bddc215864b0e6fa7efe7d261df3019831543f-"]' share.queue.local.syncs.png
 
 # chart for share.queue.remote.syncs
 echo "getting chart for share.queue.remote.syncs"
-./createlc.py '[("share.queue.remote.syncs",[100])]' '["92bddc215864b0e6fa7efe7d261df3019831543f-"]' share.queue.remote.syncs.png
+./createlc.py '[("share.queue.remote.syncs",[100,100])]' '["92bddc215864b0e6fa7efe7d261df3019831543f-","92bddc215864b0e6fa7efe7d261df3019831543f-"]' share.queue.remote.syncs.png
 
 # chart for share.single.local.syncs
 echo "getting chart for share.single.local.syncs"
-./createlc.py '[("share.single.local.syncs",[100])]' '["92bddc215864b0e6fa7efe7d261df3019831543f-"]' share.single.local.syncs.png
+./createlc.py '[("share.single.local.syncs",[100,100])]' '["92bddc215864b0e6fa7efe7d261df3019831543f-","92bddc215864b0e6fa7efe7d261df3019831543f-"]' share.single.local.syncs.png
 
 # chart for share.single.remote.syncs
 echo "getting chart for share.single.remote.syncs"
-./createlc.py '[("share.single.remote.syncs",[100])]' '["92bddc215864b0e6fa7efe7d261df3019831543f-"]' share.single.remote.syncs.png
+./createlc.py '[("share.single.remote.syncs",[100,100])]' '["92bddc215864b0e6fa7efe7d261df3019831543f-","92bddc215864b0e6fa7efe7d261df3019831543f-"]' share.single.remote.syncs.png
 
 # chart for range.access
 echo "getting chart for range.access"
-./createlc.py '[("range.access",[9223372036854775807])]' '["92bddc215864b0e6fa7efe7d261df3019831543f-"]' range.access.png
+./createlc.py '[("range.access",[9223372036854775807,9223372036854775807])]' '["92bddc215864b0e6fa7efe7d261df3019831543f-","92bddc215864b0e6fa7efe7d261df3019831543f-"]' range.access.png
 
 # chart for random.gaussianLong.generator
 echo "getting chart for random.gaussianLong.generator"
-./createlc.py '[("random.gaussianLong.generator",[9223372036854775807])]' '["92bddc215864b0e6fa7efe7d261df3019831543f-"]' random.gaussianLong.generator.png
+./createlc.py '[("random.gaussianLong.generator",[9223372036854775807,9223372036854775807])]' '["92bddc215864b0e6fa7efe7d261df3019831543f-","92bddc215864b0e6fa7efe7d261df3019831543f-"]' random.gaussianLong.generator.png
 
 # chart for random.randomDouble.generator
 echo "getting chart for random.randomDouble.generator"
-./createlc.py '[("random.randomDouble.generator",[9223372036854775807])]' '["92bddc215864b0e6fa7efe7d261df3019831543f-"]' random.randomDouble.generator.png
+./createlc.py '[("random.randomDouble.generator",[9223372036854775807,9223372036854775807])]' '["92bddc215864b0e6fa7efe7d261df3019831543f-","92bddc215864b0e6fa7efe7d261df3019831543f-"]' random.randomDouble.generator.png
 
 # chart for random.randomLong.generator
 echo "getting chart for random.randomLong.generator"
-./createlc.py '[("random.randomLong.generator",[9223372036854775807])]' '["92bddc215864b0e6fa7efe7d261df3019831543f-"]' random.randomLong.generator.png
+./createlc.py '[("random.randomLong.generator",[9223372036854775807,9223372036854775807])]' '["92bddc215864b0e6fa7efe7d261df3019831543f-","92bddc215864b0e6fa7efe7d261df3019831543f-"]' random.randomLong.generator.png
 
 # chart for random.randomInt.generator
 echo "getting chart for random.randomInt.generator"
-./createlc.py '[("random.randomInt.generator",[9223372036854775807])]' '["92bddc215864b0e6fa7efe7d261df3019831543f-"]' random.randomInt.generator.png
+./createlc.py '[("random.randomInt.generator",[9223372036854775807,9223372036854775807])]' '["92bddc215864b0e6fa7efe7d261df3019831543f-","92bddc215864b0e6fa7efe7d261df3019831543f-"]' random.randomInt.generator.png
 
 # chart for property.resolution
 echo "getting chart for property.resolution"
-./createlc.py '[("property.resolution",[9223372036854775807])]' '["92bddc215864b0e6fa7efe7d261df3019831543f-"]' property.resolution.png
+./createlc.py '[("property.resolution",[9223372036854775807,9223372036854775807])]' '["92bddc215864b0e6fa7efe7d261df3019831543f-","92bddc215864b0e6fa7efe7d261df3019831543f-"]' property.resolution.png
 
 # chart for function.calls
 echo "getting chart for function.calls"
-./createlc.py '[("function.calls",[33333.333])]' '["92bddc215864b0e6fa7efe7d261df3019831543f-"]' function.calls.png
+./createlc.py '[("function.calls",[33333.333,33333.333])]' '["92bddc215864b0e6fa7efe7d261df3019831543f-","92bddc215864b0e6fa7efe7d261df3019831543f-"]' function.calls.png
 
 # chart for choices
 echo "getting chart for choices"
-./createlc.py '[("choices",[49999.998])]' '["92bddc215864b0e6fa7efe7d261df3019831543f-"]' choices.png
+./createlc.py '[("choices",[49999.998,49999.998])]' '["92bddc215864b0e6fa7efe7d261df3019831543f-","92bddc215864b0e6fa7efe7d261df3019831543f-"]' choices.png
 
 # chart for stats.txt.calcs
 echo "getting chart for stats.txt.calcs"
-./createlc.py '[("stats.txt.calcs",[9223372036854775807])]' '["92bddc215864b0e6fa7efe7d261df3019831543f-"]' stats.txt.calcs.png
+./createlc.py '[("stats.txt.calcs",[9223372036854775807,9223372036854775807])]' '["92bddc215864b0e6fa7efe7d261df3019831543f-","92bddc215864b0e6fa7efe7d261df3019831543f-"]' stats.txt.calcs.png
 
 # chart for query.txt.queries
 echo "getting chart for query.txt.queries"
-./createlc.py '[("query.txt.queries",[11000])]' '["92bddc215864b0e6fa7efe7d261df3019831543f-"]' query.txt.queries.png
+./createlc.py '[("query.txt.queries",[11000,3000])]' '["92bddc215864b0e6fa7efe7d261df3019831543f-","92bddc215864b0e6fa7efe7d261df3019831543f-"]' query.txt.queries.png
 
 # chart for recorder.csv.writes
 echo "getting chart for recorder.csv.writes"
-./createlc.py '[("recorder.csv.writes",[100])]' '["92bddc215864b0e6fa7efe7d261df3019831543f-"]' recorder.csv.writes.png
+./createlc.py '[("recorder.csv.writes",[100,100])]' '["92bddc215864b0e6fa7efe7d261df3019831543f-","92bddc215864b0e6fa7efe7d261df3019831543f-"]' recorder.csv.writes.png
 
 # chart for recorder.txt.writes
 echo "getting chart for recorder.txt.writes"
-./createlc.py '[("recorder.txt.writes",[100])]' '["92bddc215864b0e6fa7efe7d261df3019831543f-"]' recorder.txt.writes.png
+./createlc.py '[("recorder.txt.writes",[100,100])]' '["92bddc215864b0e6fa7efe7d261df3019831543f-","92bddc215864b0e6fa7efe7d261df3019831543f-"]' recorder.txt.writes.png
 
 # chart for events.local
 echo "getting chart for events.local"
-./createlc.py '[("events.local",[124.751])]' '["92bddc215864b0e6fa7efe7d261df3019831543f-"]' events.local.png
+./createlc.py '[("events.local",[124.751,120.656])]' '["92bddc215864b0e6fa7efe7d261df3019831543f-","92bddc215864b0e6fa7efe7d261df3019831543f-"]' events.local.png
 
 # chart for events.remote
 echo "getting chart for events.remote"
-./createlc.py '[("events.remote",[1587.302])]' '["92bddc215864b0e6fa7efe7d261df3019831543f-"]' events.remote.png
+./createlc.py '[("events.remote",[1587.302,1724.138])]' '["92bddc215864b0e6fa7efe7d261df3019831543f-","92bddc215864b0e6fa7efe7d261df3019831543f-"]' events.remote.png
 
 # chart for arithmetic.abs
 echo "getting chart for arithmetic.abs"
-./createlc.py '[("arithmetic.abs",[11111.112])]' '["92bddc215864b0e6fa7efe7d261df3019831543f-"]' arithmetic.abs.png
+./createlc.py '[("arithmetic.abs",[11111.112,3571.428])]' '["92bddc215864b0e6fa7efe7d261df3019831543f-","92bddc215864b0e6fa7efe7d261df3019831543f-"]' arithmetic.abs.png
 
 # chart for arithmetic.mul
 echo "getting chart for arithmetic.mul"
-./createlc.py '[("arithmetic.mul",[1960.784])]' '["92bddc215864b0e6fa7efe7d261df3019831543f-"]' arithmetic.mul.png
+./createlc.py '[("arithmetic.mul",[1960.784,2777.778])]' '["92bddc215864b0e6fa7efe7d261df3019831543f-","92bddc215864b0e6fa7efe7d261df3019831543f-"]' arithmetic.mul.png
 
 # chart for arithmetic.div
 echo "getting chart for arithmetic.div"
-./createlc.py '[("arithmetic.div",[1886.792])]' '["92bddc215864b0e6fa7efe7d261df3019831543f-"]' arithmetic.div.png
+./createlc.py '[("arithmetic.div",[1886.792,2325.581])]' '["92bddc215864b0e6fa7efe7d261df3019831543f-","92bddc215864b0e6fa7efe7d261df3019831543f-"]' arithmetic.div.png
 
 # chart for arithmetic.sub
 echo "getting chart for arithmetic.sub"
-./createlc.py '[("arithmetic.sub",[1639.344])]' '["92bddc215864b0e6fa7efe7d261df3019831543f-"]' arithmetic.sub.png
+./createlc.py '[("arithmetic.sub",[1639.344,11111.112])]' '["92bddc215864b0e6fa7efe7d261df3019831543f-","92bddc215864b0e6fa7efe7d261df3019831543f-"]' arithmetic.sub.png
 
 # chart for arithmetic.mod
 echo "getting chart for arithmetic.mod"
-./createlc.py '[("arithmetic.mod",[11111.112])]' '["92bddc215864b0e6fa7efe7d261df3019831543f-"]' arithmetic.mod.png
+./createlc.py '[("arithmetic.mod",[11111.112,20000.000])]' '["92bddc215864b0e6fa7efe7d261df3019831543f-","92bddc215864b0e6fa7efe7d261df3019831543f-"]' arithmetic.mod.png
 
 # chart for arithmetic.add
 echo "getting chart for arithmetic.add"
-./createlc.py '[("arithmetic.add",[1785.714])]' '["92bddc215864b0e6fa7efe7d261df3019831543f-"]' arithmetic.add.png
+./createlc.py '[("arithmetic.add",[1785.714,1492.537])]' '["92bddc215864b0e6fa7efe7d261df3019831543f-","92bddc215864b0e6fa7efe7d261df3019831543f-"]' arithmetic.add.png
