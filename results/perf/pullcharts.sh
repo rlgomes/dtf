62129893aaa563f4152051095bf37d97453589f0
dtftest <dtftest@localhost.(none)>
2011-01-01 08:28:28 +0000

    Test results for commit 92bddc215864b0e6fa7efe7d261df3019831543f14452 on 01-01-2011.08:27:54

--- results/perf/pullcharts.sh
+++ results/perf/pullcharts.sh
@@ -3,24 +3,24 @@
 
 # chart for arithmetic.abs
 echo "getting chart for arithmetic.abs"
-./createlc.py '[("arithmetic.abs",[4545.455,1315.790,3571.428])]' '["92bddc215864b0e6fa7efe7d261df3019831543f11714","92bddc215864b0e6fa7efe7d261df3019831543f30461","92bddc215864b0e6fa7efe7d261df3019831543f29514"]' arithmetic.abs.png
+./createlc.py '[("arithmetic.abs",[4545.455,1315.790,3571.428,1960.784])]' '["92bddc215864b0e6fa7efe7d261df3019831543f11714","92bddc215864b0e6fa7efe7d261df3019831543f30461","92bddc215864b0e6fa7efe7d261df3019831543f29514","92bddc215864b0e6fa7efe7d261df3019831543f14452"]' arithmetic.abs.png
 
 # chart for arithmetic.mul
 echo "getting chart for arithmetic.mul"
-./createlc.py '[("arithmetic.mul",[5555.556,6250.000,3571.428])]' '["92bddc215864b0e6fa7efe7d261df3019831543f11714","92bddc215864b0e6fa7efe7d261df3019831543f30461","92bddc215864b0e6fa7efe7d261df3019831543f29514"]' arithmetic.mul.png
+./createlc.py '[("arithmetic.mul",[5555.556,6250.000,3571.428,1666.667])]' '["92bddc215864b0e6fa7efe7d261df3019831543f11714","92bddc215864b0e6fa7efe7d261df3019831543f30461","92bddc215864b0e6fa7efe7d261df3019831543f29514","92bddc215864b0e6fa7efe7d261df3019831543f14452"]' arithmetic.mul.png
 
 # chart for arithmetic.div
 echo "getting chart for arithmetic.div"
-./createlc.py '[("arithmetic.div",[3030.303,1818.182,2439.024])]' '["92bddc215864b0e6fa7efe7d261df3019831543f11714","92bddc215864b0e6fa7efe7d261df3019831543f30461","92bddc215864b0e6fa7efe7d261df3019831543f29514"]' arithmetic.div.png
+./createlc.py '[("arithmetic.div",[3030.303,1818.182,2439.024,9090.909])]' '["92bddc215864b0e6fa7efe7d261df3019831543f11714","92bddc215864b0e6fa7efe7d261df3019831543f30461","92bddc215864b0e6fa7efe7d261df3019831543f29514","92bddc215864b0e6fa7efe7d261df3019831543f14452"]' arithmetic.div.png
 
 # chart for arithmetic.sub
 echo "getting chart for arithmetic.sub"
-./createlc.py '[("arithmetic.sub",[5263.158,4761.905,6250.000])]' '["92bddc215864b0e6fa7efe7d261df3019831543f11714","92bddc215864b0e6fa7efe7d261df3019831543f30461","92bddc215864b0e6fa7efe7d261df3019831543f29514"]' arithmetic.sub.png
+./createlc.py '[("arithmetic.sub",[5263.158,4761.905,6250.000,1639.344])]' '["92bddc215864b0e6fa7efe7d261df3019831543f11714","92bddc215864b0e6fa7efe7d261df3019831543f30461","92bddc215864b0e6fa7efe7d261df3019831543f29514","92bddc215864b0e6fa7efe7d261df3019831543f14452"]' arithmetic.sub.png
 
 # chart for arithmetic.mod
 echo "getting chart for arithmetic.mod"
-./createlc.py '[("arithmetic.mod",[3846.154,6666.667,20000.000])]' '["92bddc215864b0e6fa7efe7d261df3019831543f11714","92bddc215864b0e6fa7efe7d261df3019831543f30461","92bddc215864b0e6fa7efe7d261df3019831543f29514"]' arithmetic.mod.png
+./createlc.py '[("arithmetic.mod",[3846.154,6666.667,20000.000,9090.909])]' '["92bddc215864b0e6fa7efe7d261df3019831543f11714","92bddc215864b0e6fa7efe7d261df3019831543f30461","92bddc215864b0e6fa7efe7d261df3019831543f29514","92bddc215864b0e6fa7efe7d261df3019831543f14452"]' arithmetic.mod.png
 
 # chart for arithmetic.add
 echo "getting chart for arithmetic.add"
-./createlc.py '[("arithmetic.add",[1408.451,1960.784,1754.386])]' '["92bddc215864b0e6fa7efe7d261df3019831543f11714","92bddc215864b0e6fa7efe7d261df3019831543f30461","92bddc215864b0e6fa7efe7d261df3019831543f29514"]' arithmetic.add.png
+./createlc.py '[("arithmetic.add",[1408.451,1960.784,1754.386,1612.903])]' '["92bddc215864b0e6fa7efe7d261df3019831543f11714","92bddc215864b0e6fa7efe7d261df3019831543f30461","92bddc215864b0e6fa7efe7d261df3019831543f29514","92bddc215864b0e6fa7efe7d261df3019831543f14452"]' arithmetic.add.png
